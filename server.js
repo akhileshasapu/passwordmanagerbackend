32ace6824e5c660b9fe69d18ec08c503a21c9b73
akhileshasapu <156326254+akhileshasapu@users.noreply.github.com>
2025-06-27 18:23:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
-import express from 'express';
+import express from 'express'
 import cors from 'cors';
 import mongoose from 'mongoose';
-
-import authroute from './routes/auth.js';
+import authroute from './routes/auth.js'
 import Manageroute from './routes/manage.js';
 
-const app = express();
-
-// Only allow your Vercel frontend
-const allowedOrigins = ['https://pass-o-ppasswordmanager.vercel.app'];
-
+const app = express()
+const allowedOrigins = [
+ 'https://pass-o-ppasswordmanager.vercel.app'
+];
 app.use(cors({
   origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
@@ -21,24 +18,20 @@ app.use(cors({
       callback(new Error("Not allowed by CORS"));
     }
   },
-  credentials: true, // If you're sending cookies or authorization headers
+  credentials: true,
 }));
 
-// Allow preflight (OPTIONS) requests
-app.options("*", cors());
-
-app.use(express.json());
-
+app.use(express.json())
 mongoose.connect(process.env.mongo_url)
-  .then(() => {
-    console.log("Database connected");
-  })
-  .catch((err) => {
-    console.log("Error is ", err);
-  });
+.then(()=>{
+    console.log("database connected")
+})
+.catch((err)=>{
+ console.log("error is ", err);
+})
 
-app.use("/api/auth", authroute);
-app.use("/api", Manageroute);
+app.use("/api/auth",authroute)
+app.use("/api/",Manageroute)
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server listening at ${port}`));
+const port = 3000;
+app.listen(port,()=> console.log(`server listening at ${port}`))
